69d85087f2d672c2ef3c7baec45884a0e24de5df
Buggie <jsurgeon29@gmail.com>
2021-05-20 15:09:27 -0700

    updated tables to include keys; finished postgres queries

--- EmployeeSQL/tables.sql
+++ EmployeeSQL/tables.sql
@@ -1,37 +1,75 @@
--- tables.sql file used to create tables for sql-challenge
--- Jonathah Surgeon 5/18/2021
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE dept_manager (
-	dept_no varchar(100) NOT NULL,
-	emp_no int NOT NULL
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(200)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
-CREATE TABLE departments (
-	dept_no varchar(100) NOT NULL,
-	dept_name varchar(100) NOT NULL
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(200)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(200)   NOT NULL,
+    "last_name" varchar(200)   NOT NULL,
+    "sex" char   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
-CREATE TABLE dept_emp (
-	emp_no int NOT NULL,
-	dept_no varchar(100) NOT NULL
+CREATE TABLE "departments" (
+    "dept_no" varchar(200)   NOT NULL,
+    "dept_name" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
-CREATE TABLE employees (
-	emp_no int PRIMARY KEY,
-	emp_title_id varchar(100),
-	birth_date date,
-	first_name varchar(100),
-	last_name varchar(100),
-	sex char,
-	hire_date date
+CREATE TABLE "titles" (
+    "title_id" varchar(20)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
-CREATE TABLE titles (
-	title_id varchar(100),
-	title varchar(50)
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
-CREATE TABLE salaries (
-	emp_no int NOT NULL,
-	salary int NOT NULL
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
